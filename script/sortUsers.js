f2bff114e87cd6d887668b1fcda9af4a884ec85e
Dunderblixt <stalhane@gmail.com>
2026-03-27 15:52:45 +0100

    Delete sortUsers.js

--- script/sortUsers.js
+++
@@ -1,7 +0,0 @@
-export function sortUsersByCreatedAt(users) {
-  if (!users) return [];
-
-  return Object.entries(users).sort(([, user1], [, user2]) => {
-    return (user2.createdAt || 0) - (user1.createdAt || 0);
-  });
-}
